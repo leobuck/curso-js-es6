2dcc07f235fa592c31e4c6f9beffe0342bdb560c
leobuck <leobuck12@gmail.com>
2020-08-05 17:34:25 -0300

    primeiro commit

---
+++ src/main_webpack.js
@@ -0,0 +1,7 @@
+//import soma, { sub } from './funcoes';
+import * as funcoes from './funcoes';
+//import somaFunction from './soma';
+
+//console.log(soma(1, 2));
+//console.log(sub(4, 2));
+console.log(funcoes.soma(1, 2));
